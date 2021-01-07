aa5686dcac7b2b2045f0654bbbe355c4fde62e58
Shota Akizuki <64979430+shota-akizuki@users.noreply.github.com>
2021-01-07 15:09:16 +0900

    クラスコンポーネントを使った書き換え

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React, { useState } from "react";
 import List from "./List";
-
-//属性＝{}でJavaScriptの要素を代入
-//通常のhtmlとJSXの違いは...
-//class => className
-//onclick => onClick
-export const App = () => {
-  const [description, setDescription] = useState("クリック前の表示");
-  //状態を変更するための関数を定義
-  //関数内でsetDescriptionを使用する
-  const changeDescription = () => {
-    setDescription("クリック後の表示です");
-  };
-  return (
-    <div>
-      {description}
-      <List title="取り扱い言語一覧" />
-      <button onClick={changeDescription}>ボタン</button>
-    </div>
-  );
-};
+class App extends React.Component {
+  //クラスコンポーネントでstateを扱う場合はthis.stateに初期値を代入する
+  constructor(props) {
+    super(props);
+    this.state = { description: "クリック前の表示" };
+  }
+  //stateを変更するための関数を定義
+  //setStateはオブジェクトを受け取り、変更後の値を受け取る（setdescriptionみたいな関数はない）
+  changeDescription() {
+    this.setState({
+      description: "クリック後の表示",
+    });
+  }
+  render() {
+    //stateをdescriptionから取り出して表示する
+    const { description } = this.state;
+    //クラスのメソッドを呼ぶために、アロー関数でthisを引き継ぐようにしている
+    //関数コンポーネントの場合は関数の参照だけ渡している
+    //this.changeDescription()だけだと動かない
+    //this.changeDescription().bind(this)でも動く
+    return (
+      <div>
+        {description}
+        <List title="取り扱い言語一覧" />
+        <button onClick={() => this.changeDescription()}>ボタン</button>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-export const List = ({ title }) => {
-  return (
-    <div>
-      <p>{title}</p>
-      <div>これはリストです</div>
-    </div>
-  );
-};
-
+class List extends React.Component {
+  //propsはrenderメソッドの中に書く
+  render() {
+    const { title } = this.props;
+    return (
+      <div>
+        <p>{title}</p>
+        <div>リストです</div>
+      </div>
+    );
+  }
+}
 export default List;
