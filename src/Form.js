b8141f3b3a63c212f5c96c0d1d39b88d5975f470
Shota Akizuki <64979430+shota-akizuki@users.noreply.github.com>
2021-01-07 23:32:15 +0900

    フォームの修正

--- src/Form.js
+++ src/Form.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Form = () => {
-  return <div>フォームです</div>;
+  const [text, settext] = useState("");
+  const submitForm = (event) => {
+    //eventを受け取った時に、ページ遷移が走ってしまわないように
+    event.preventDefault();
+    console.log(text);
+  };
+
+  return (
+    <div>
+      <h4>新しい言語の追加</h4>
+      <form
+        //基本的にformの中ではonSubmitを使う（buttonに処理しない）
+        onSubmit={submitForm}
+      >
+        <div>
+          <input
+            //Reactでinputで反映する時の処理
+            //buttonにonClick処理にしてしまうと、Enterに反応しない
+            //onChange={(e) => settext(e.target.value)でeventを受け取れる
+            type="text"
+            value={text}
+            onChange={(e) => settext(e.target.value)}
+          />
+        </div>
+        <div>
+          <button>追加</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Form;
